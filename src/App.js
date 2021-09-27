be1eb02e4c259ef5714b0f81e3e1c2e7344b36b0
Khalil Saad Harouri <saadharouri@gmail.com>
2021-09-27 17:15:05 +0100

    Added checkout component and linking it with router

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {createStructuredSelector} from 'reselect';
 import {selectCurrentUser} from './redux/user/user.selectors';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
@@ -52,6 +53,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           <Route exact path='/signin' render={() => this.props.currentUser ? 
             (<Redirect to='/'/>
             ) : (<SignInAndSignUpPage />)
